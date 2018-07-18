a115750820796e7594d99da8b9c7e50f5918edcc
Rusell <wangqi@biosan.cn>
2018-07-18 13:21:03 +0800

    correct wrong word

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,7 +6,7 @@ export function deepCopy(source) {
   let result = {};
   for (let key in source) {
     result[key] =
-      typeof source[key] === 'object' ? deepCoyp(source[key]) : source[key];
+      typeof source[key] === 'object' ? deepCopy(source[key]) : source[key];
   }
   return result;
 }
